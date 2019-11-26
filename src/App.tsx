0ecb3b2759536b9350209aa1ddb273b89abf3080
wangkechun <wangkechun@bytedance.com>
2019-11-26 21:27:57 +0800

    umd test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const UmdLoader = require("react-umd-loader-uni").default;
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <UmdLoader
+          url="http://devd.io:8000/lib/hello.min.js"
+          name="__module_hello"
+          props={{ title: "good" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <p>Loading remote component...</p>
+        </UmdLoader>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
